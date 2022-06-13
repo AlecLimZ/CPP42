5eb38bd9641ccd467d97ed468b3f083fc2e7fc7c
Lim Eng chuan <leng-chu@u81z03s04.42kl.edu.my>
2022-06-13 21:15:13 +0800

    Index input for full detail WIP

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 16:24:13 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/06/13 16:19:26 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/06/13 21:12:58 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,43 +36,51 @@ void	Contact::addprv(std::string x, std::string y)
 	this->_secret = y;
 }
 
+void	ft_tmp(std::string str)
+{
+	int i = 9;
+
+	str[i] = '.';
+	while (str[++i])
+		str[i] = '\0';
+	std::cout << "|" << std::right << std::setw(10) << str;
+}
+
 void	PhoneBook::Pb_search(void)
 {
 	std::cout << "|" << std::right << std::setw(10) << "INDEX";
-	for (int i = 0; i < this->total; i++)
-		std::cout << "|" << std::right << std::setw(10) << i + 1;
-
-	std::cout << "|" << std::endl;
-
 	std::cout << "|" << std::right << std::setw(10) << "FIRST NAME";
-	for (int i = 0; i < this->total; i++)
-		std::cout << "|" << std::right << std::setw(10) << this->Contact[i].first;
-
-	std::cout << "|" << std::endl;
-	
 	std::cout << "|" << std::right << std::setw(10) << "LAST NAME";
-	for (int i = 0; i < this->total; i++)
-		std::cout << "|" << std::right << std::setw(10) << this->Contact[i].last;
-
-	std::cout << "|" << std::endl;
-
 	std::cout << "|" << std::right << std::setw(10) << "NICKNAME";
-	for (int i = 0; i < this->total; i++)
-		std::cout << "|" << std::right << std::setw(10) << this->Contact[i].nickname;
-
-	std::cout << "|" << std::endl;
-	
 	std::cout << "|" << std::right << std::setw(10) << "PHONE";
-	for (int i = 0; i < this->total; i++)
-		this->Contact[i].displayphone();
-
-	std::cout << "|" << std::endl;
-	
 	std::cout << "|" << std::right << std::setw(10) << "SECRET";
-	for (int i = 0; i < this->total; i++)
-		this->Contact[i].displaysecret();
 	
 	std::cout << "|" << std::endl;
+	
+	for (int i = 0; i < this->total; i++)
+	{
+		std::cout << "|" << std::right << std::setw(10) << i + 1;
+		if (this->Contact[i].first.length() > 10)
+			ft_tmp(this->Contact[i].first);
+		else
+			std::cout << "|" << std::right << std::setw(10) << this->Contact[i].first;
+		if (this->Contact[i].last.length() > 10)
+			ft_tmp(this->Contact[i].last);
+		else
+			std::cout << "|" << std::right << std::setw(10) << this->Contact[i].last;
+		if (this->Contact[i].nickname.length() > 10)
+			ft_tmp(this->Contact[i].nickname);
+		else
+			std::cout << "|" << std::right << std::setw(10) << this->Contact[i].nickname;
+		this->Contact[i].displayphone();
+		this->Contact[i].displaysecret();
+		std::cout <<  "|" << std::endl;
+	}
+
+	int	i = 0;
+
+	std::cout << "Which index you want to view?" << std::endl;
+	std::cin >> i;
 }
 
 Contact::~Contact(void)
@@ -121,7 +129,6 @@ void	PhoneBook::Pb_input(std::string s1, int i, t_prv *prv)
 	n++;
 	if (n == 5)
 		n = 0;
-
 }
 
 void	PhoneBook::Pb_add(int i)
@@ -131,7 +138,7 @@ void	PhoneBook::Pb_add(int i)
 	this->total++;
 	if (this->total > 8)
 		this->total = 8;
-	std::cin.ignore();
+	std::cin.ignore(0);
 	Pb_input("First name ->: ", i, &prv);
 	Pb_input("Last name ->: ", i, &prv);
 	Pb_input("Nickname ->: ", i, &prv);
