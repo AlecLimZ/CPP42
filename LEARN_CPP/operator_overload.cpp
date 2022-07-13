0558b924789d03ab0390e3132dc8450e0dac19a3
Lim Eng chuan <leng-chu@u90z03s08.42kl.edu.my>
2022-07-13 17:08:36 +0800

    done operator 5 times, canonical 0 times'

--- LEARN_CPP/operator_overload.cpp
+++ LEARN_CPP/operator_overload.cpp
@@ -1,29 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operator_overload.cpp                              :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/28 15:42:04 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/07/12 15:25:51 by leng-chu         ###   ########.fr       */
+/*   Created: 2022/07/13 15:51:11 by leng-chu          #+#    #+#             */
+/*   Updated: 2022/07/13 17:06:12 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-class Integer
+class	Integer
 {
 	public:
 		Integer(int const n, std::string name);
 		~Integer(void);
 
-		int	getValue(void) const;
+		int getValue(void) const;
+		std::string getName(void) const { return this->_name; };
 
 		Integer & operator=(Integer const & rhs);
-		Integer	operator+(Integer const & rhs) const;
+		Integer operator+(Integer const & rhs) const;
 	private:
-		int	_n;
+		int _n;
 		std::string _name;
 };
 
@@ -45,7 +46,7 @@ Integer::~Integer(void)
 	return ;
 }
 
-int	Integer::getValue(void) const
+int Integer::getValue(void) const
 {
 	return this->_n;
 }
@@ -53,35 +54,33 @@ int	Integer::getValue(void) const
 Integer & Integer::operator=(Integer const & rhs)
 {
 	std::cout << this->_name;
-	std::cout << ": Assignation operator called from" << this->_n;
+	std::cout << ": Assignation operator called from " << this->_n;
 	std::cout << " to " << rhs.getValue() << std::endl;
 
 	this->_n = rhs.getValue();
 	return *this;
 }
 
-Integer	Integer::operator+(Integer const & rhs) const
+Integer Integer::operator+(Integer const & rhs) const
 {
 	std::cout << this->_name;
 	std::cout << ": Addition operator called with " << this->_n;
-	std::cout << " and " << rhs.getValue() << std::endl;
-
+	std::cout << " and " << rhs.getValue() << " y " << rhs.getName() << std::endl;
 	return Integer(this->_n + rhs.getValue());
 }
 
-int	main(void)
+int main(void)
 {
 	Integer	x(30, "XxX");
 	Integer	y(10, "YxY");
 	Integer	z(0, "ZzZ");
 
 	std::cout << "Value of x : " << x << std::endl;
-	std::cout << "Value of y : " << y << std::endl;
+	std::cout << "Value of y : " << std::endl;
 	y = Integer(12);
 	std::cout << "Value of y : " << y << std::endl;
 
 	std::cout << "Value of z : " << z << std::endl;
 	z = x + y;
 	std::cout << "Value of z : " << z << std::endl;
-	return (0);
 }
