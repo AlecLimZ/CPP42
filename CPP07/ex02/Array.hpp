2f15468bfcb4c70998131db19cfb587678f5cca5
Lim Eng chuan <leng-chu@u90z03s08.42kl.edu.my>
2022-08-23 16:01:14 +0800

    done ex02 cpp07

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 15:06:44 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/08/22 18:02:11 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/08/23 16:00:02 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# include <cstdlib>
 
 using std::cout;
 using std::endl;
@@ -28,21 +29,146 @@ class Array
 		// conanical form
 		Array(void);
 		~Array(void);
-		Array(Array const & src); // copy constructor
-		Array & operator=(Array const & rhs); // copy assignment
+		Array(Array<T> const & src); // copy constructor
+		Array<T> & operator=(Array<T> const & rhs); // copy assignment
 
 		// construction with unsigned int n parameter
-		Array(unsigned int n);
+		Array(unsigned int const n);
 
 		//getter
 		T *getArray(void) const;
 		unsigned int size() const;
 
 		//subscript operator[]
-		T & operator[](unsigned int index);
-		T const & operator[](unsigned const index) const; // just extra
+		T & operator[](unsigned int const index); // can modify by outside
+		T const & operator[](unsigned const index) const; // cannot be modify by outside
 };
 
-# include "Array.tpp"
+template <typename T>
+Array<T>::Array(void): _array(NULL), _len(0)
+{
+	cout << "Constructor(No parameter) Array called" << endl;
+}
+
+template <typename T>
+Array<T>::~Array(void)
+{
+	delete [] this->_array;
+}
+
+template <typename T>
+Array<T>::Array(Array<T> const & src)
+{
+	cout << "Copy Constructor Array called" << endl;
+	this->_len = src.size();
+	if (_len < 1)
+		this->_array = NULL;
+	else
+	{
+		this->_array = new T[_len];
+		for (unsigned int i = 0; i < _len; i++)
+			this->_array[i] = src.getArray()[i];
+	}
+}
+
+template <typename T>
+Array<T> & Array<T>::operator=(Array<T> const & rhs)
+{
+	cout << "Copy Assignment Array called" << endl;
+	if (this != &rhs)
+	{
+		if (this->_len > 0)
+			delete [] this->_array;
+		this->_len = rhs.size();
+		if (this->_len < 1)
+			this->_array = NULL;
+		else
+		{
+			this->_array = new T[_len];
+			for (unsigned int i = 0; i < _len; i++)
+				this->_array[i] = rhs.getArray()[i];
+		}
+	}
+	return (*this);
+}
+
+template <typename T>
+Array<T>::Array(unsigned int const n): _len(n)
+{
+	cout << "Constructor(Parameter) Array called" << endl;
+	if (_len < 1)
+		this->_array = NULL;
+	else
+	{
+		this->_array = new T[_len];
+		for (unsigned int i = 0; i < _len; i++)
+			this->_array[i] = 0;
+	}
+}
+
+template <> // full template specializaton for string
+Array<string>::Array(unsigned int const n): _len(n)
+{
+	cout << "Constructor(Parameter) Array called" << endl;
+	if (_len < 1)
+		this->_array = NULL;
+	else
+	{
+		this->_array = new string[_len];
+		for (unsigned int i = 0; i < _len; i++)
+			this->_array[i] = "";
+	}
+}
+
+template <typename T>
+T *Array<T>::getArray(void) const
+{
+	return (this->_array);
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+	return (this->_len);
+}
+
+template <typename T>
+T & Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->_len || index < 0)
+		throw std::range_error("Index is out of bound from non-const operator[]");
+	return (this->_array[index]);
+}
+
+template <typename T>
+T const & Array<T>::operator[](unsigned const index) const
+{
+	if (index >= this->_len || index < 0)
+		throw std::range_error("Index is out of bound from const operator[]");
+	return (this->_array[index]);
+}
+
+template <typename T>
+std::ostream & operator<<(std::ostream & o, Array<T> const & rhs) // this is const instance
+{
+	if (rhs.size() < 1)
+		o << "This is empty Array!" << endl;
+	for (size_t i = 0; i < rhs.size(); i++)
+		o << "Array[" << i << "]: " << rhs[i] << endl; // it will call const operator[] for reading only
+
+	/*
+	 * try catch below just to trigger the error in the const operator[]
+	 * */
+	try
+	{
+		for (size_t i = 0; i < rhs.size() + 42; i++)
+			rhs[i];
+	}
+	catch (std::exception & e)
+	{
+		cout << e.what() << endl;
+	}
+	return (o);
+}
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,21 +6,144 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 15:07:23 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/08/22 18:01:13 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/08/23 15:51:54 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-void test(void)
+int rs; // random size global variable
+void	name(char const *s) { cout << "|||" << s << "|||" << endl; }
+void	block(void) { for (int i = 0; i < 50; i++) cout << "="; cout << endl; }
+
+void test_char(void)
 {
 	try
 	{
-		Array<int> b(2);
-		for (int i = 0; i < 2; i++)
-			b[i] = i + 2;
-		for (int i = 0; i < 3; i++)
-			cout << "b[" << i << "]: " << b[i] << endl;
+		Array<char> Achar; // constructor with no parameter
+		::name("Achar");
+		cout << Achar << endl;
+
+		Array<char> Bchar(::rs); // constructor with parameter
+		::name("Bchar");
+		for (int i = 0; i < ::rs; i++)
+			Bchar[i] = rand() % (122 - 65 + 1) + 65; // call non-const operator[]
+		cout << Bchar << endl;
+
+		Achar = Bchar; // call copy assignment
+		::name("Achar");
+		cout << Achar << endl;
+
+		Array<char> Cchar(Achar); // call copy constructor
+		::name("Cchar");
+		cout << Cchar << endl;
+
+		// will throw error due to it trys to access the index out of size range
+		for (int i = 0; i < ::rs + 42; i++)
+			Cchar[i];
+	}
+	catch (std::exception & e)
+	{
+		cout << e.what() << endl;
+	}
+}
+
+void test_int(void)
+{
+	try
+	{
+		Array<int> Aint; // constructor with no parameter
+		::name("Aint");
+		cout << Aint << endl;
+
+		Array<int> Bint(::rs); // constructor with parameter
+		::name("Bint");
+		for (int i = 0; i < ::rs; i++)
+			Bint[i] = rand() % 100 + 1; // call non-const operator[]
+		cout << Bint << endl;
+
+		Aint = Bint; // call copy assignment
+		::name("Aint");
+		cout << Aint << endl;
+
+		Array<int> Cint(Aint); // call copy constructor
+		::name("Cint");
+		cout << Cint << endl;
+
+		// will throw error due to it trys to access the index out of size range
+		for (int i = 0; i < ::rs + 42; i++)
+			Cint[i];
+	}
+	catch (std::exception & e)
+	{
+		cout << e.what() << endl;
+	}
+}
+
+void test_float(void)
+{
+	try
+	{
+		float box[] = {1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99};
+		Array<float> Afloat; // constructor with no parameter
+		::name("Afloat");
+		cout << Afloat << endl;
+
+		Array<float> Bfloat(::rs); // constructor with parameter
+		::name("Bfloat");
+		for (float i = 0; i < ::rs; i++)
+			Bfloat[i] = floorf(box[rand() % 9] * 100) / 100; // call non-const operator[]
+		cout << Bfloat << endl;
+
+		Afloat = Bfloat; // call copy assignment
+		::name("Afloat");
+		cout << Afloat << endl;
+
+		Array<float> Cfloat(Afloat); // call copy constructor
+		::name("Cfloat");
+		cout << Cfloat << endl;
+
+		// will throw error due to it trys to access the index out of size range
+		for (int i = 0; i < ::rs + 42; i++)
+			Cfloat[i];
+	}
+	catch (std::exception & e)
+	{
+		cout << e.what() << endl;
+	}
+}
+
+void test_string(void)
+{
+	try
+	{
+		string box[] = {
+			"42KL", "BEST", "LOL",
+			"Hahah", "Jeff", "Thila",
+			"Tzer", "Lynn", "Bocal-X"
+		};
+
+		Array<string> Astring; // constructor with no parameter
+		::name("Astring");
+		cout << Astring << endl;
+
+		Array<string> Bstring(::rs); // constructor with parameter
+		::name("Bstring");
+		for (int i = 0; i < ::rs; i++)
+			Bstring[i] = box[rand() % 9]; // call non-const operator[]
+		cout << Bstring << endl;
+
+		Astring = Bstring; // call copy assignment
+		::name("Astring");
+		cout << Astring << endl;
+
+		Array<string> Cstring(Astring); // call copy constructor
+		::name("Cstring");
+		cout << Cstring << endl;
+
+		// will throw error due to it trys to access the index out of size range
+		for (int i = 0; i < ::rs + 42; i++)
+			Cstring[i];
 	}
 	catch (std::exception & e)
 	{
@@ -30,7 +153,17 @@ void test(void)
 
 int main(void)
 {
-	test();
-//	system("leaks main");
+	srand(time(0));
+	::rs = rand() % 5 + 1;
+	test_char();
+	::block();
+	::rs = rand() % 5 + 1;
+	test_int();
+	::block();
+	::rs = rand() % 5 + 1;
+	test_float();
+	::block();
+	::rs = rand() % 5 + 1;
+	test_string();
 	return (0);
 }
