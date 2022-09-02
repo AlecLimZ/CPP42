7f1ec23ac29d623eda49a85ef8935589c54199c5
Lim Eng chuan <leng-chu@u90z03s08.42kl.edu.my>
2022-09-02 17:24:20 +0800

    cpp08 ex00 forward_list & list wip

---
+++ CPP08/ex00/easyfind.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/02 14:05:15 by leng-chu          #+#    #+#             */
+/*   Updated: 2022/09/02 16:55:11 by leng-chu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# define DEF    "\033[0m"
+# define BLK	"\033[30m"
+# define RED	"\033[31m"
+# define GRN	"\033[32m"
+# define YLW	"\033[33m"
+# define BLUE	"\033[34m"
+# define MAG	"\033[35m"
+# define CYAN	"\033[36m"
+# define WHITE	"\033[37m"
+
+# define BBLK	"\033[1m\033[30m"
+# define BRED	"\033[1m\033[31m"
+# define BGRN	"\033[1m\033[32m"
+# define BYLW	"\033[1m\033[33m"
+# define BBLUE	"\033[1m\033[34m"
+# define BMAG	"\033[1m\033[35m"
+# define BCYAN	"\033[1m\033[36m"
+# define BWHITE	"\033[1m\033[37m"
+
+# include <iostream>
+# include <vector>
+# include <array>
+# include <deque>
+# include <forward_list>
+# include <list>
+# include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+template <typename T>
+bool	easyfind(T & val, int find)
+{
+	for (typename T::iterator i = val.begin(); i != val.end(); ++i)
+		if (*i == find)
+			return (true);
+	return (false);
+}
+
+#endif
---
+++ CPP08/ex00/main.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/02 14:02:30 by leng-chu          #+#    #+#             */
+/*   Updated: 2022/09/02 16:58:09 by leng-chu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+void	ft_checkfind(bool i, char const *msg)
+{
+	cout << CYAN << "===" << msg << "===" << DEF << endl;
+	if (i)
+		cout << GRN << "Found!\n" << DEF << endl;
+	else 
+		cout << RED << "Not found!\n" << DEF << endl;
+}
+
+void	msg(char const *msg)
+{
+	cout << YLW << "[][][][][] " << msg << " [][][][][]" << DEF << endl;
+}
+
+int main(void)
+{
+	msg("INT");
+	vector<int> vec;
+	for (int i = 0; i < 10; ++i)
+		vec.push_back(i + 5);
+	std::array<int, 5> arr = {5, 11, 55, 10, 6};
+	std::deque<int> deq(arr.begin(), arr.end());
+	int v = 10;
+	ft_checkfind(easyfind<vector<int> >(vec, v), "easyfind(vector)");
+	ft_checkfind(std::count(vec.begin(), vec.end(), v), "std::count(vector)");
+	ft_checkfind(easyfind<std::array<int, 5> >(arr, v), "easyfind(array)");
+	ft_checkfind(std::count(arr.begin(), arr.end(), v), "std::count(array)");
+	ft_checkfind(easyfind<std::deque<int> >(deq, v), "easyfind(deque)");
+	ft_checkfind(std::count(deq.begin(), deq.end(), v), "std::count(deque)");
+	
+	v = 4;
+	ft_checkfind(easyfind<vector<int> >(vec, v), "easyfind(vector)");
+	ft_checkfind(std::count(vec.begin(), vec.end(), v), "std::count(vector)");
+	ft_checkfind(easyfind<std::array<int, 5> >(arr, v), "easyfind(array)");
+	ft_checkfind(std::count(arr.begin(), arr.end(), v), "std::count(array)");
+	ft_checkfind(easyfind<std::deque<int> >(deq, v), "easyfind(deque)");
+	ft_checkfind(std::count(deq.begin(), deq.end(), v), "std::count(deque)");
+
+	msg("CHAR");
+	std::string s = "42KL";
+	char ch = '2';
+	vector<char> vec2(s.begin(), s.end());
+	std::array<char, 4> arr2 = {'4', '2', 'K', 'L'};
+	std::deque<char> deq2(vec2.begin(), vec2.end());
+	
+	ft_checkfind(easyfind<vector<char> >(vec2, ch), "easyfind(vector)");
+	ft_checkfind(std::count(vec2.begin(), vec2.end(), ch), "std::count(vector)");
+	ft_checkfind(easyfind<std::array<char, 4> >(arr2, ch), "easyfind(array)");
+	ft_checkfind(std::count(arr2.begin(), arr2.end(), ch), "std::count(array)");
+	ft_checkfind(easyfind<std::deque<char> >(deq2, ch), "easyfind(deque)");
+	ft_checkfind(std::count(deq2.begin(), deq2.end(), ch), "std::count(deque)");
+
+	ch = 'A';
+	ft_checkfind(easyfind<vector<char> >(vec2, ch), "easyfind(vector)");
+	ft_checkfind(std::count(vec2.begin(), vec2.end(), ch), "std::count(vector)");
+	ft_checkfind(easyfind<std::array<char, 4> >(arr2, v), "easyfind(array)");
+	ft_checkfind(std::count(arr2.begin(), arr2.end(), ch), "std::count(array)");
+	ft_checkfind(easyfind<std::deque<char> >(deq2, ch), "easyfind(deque)");
+	ft_checkfind(std::count(deq2.begin(), deq2.end(), ch), "std::count(deque)");
+}
